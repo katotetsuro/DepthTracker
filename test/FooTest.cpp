495bed68fa5679a142963af80769aa962bea6c98
KatoTetsuro <katotetsuro@team-lab.com>
2017-04-26 00:46:07 +0900

    clang-format実行

--- test/FooTest.cpp
+++ test/FooTest.cpp
@@ -6,12 +6,13 @@
 //
 //
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this
+                          // in one cpp file
 #include "catch.hpp"
 #include "ofApp.h"
 
-TEST_CASE( "foo will times by 2", "[foo]" ) {
-    REQUIRE( foo(1) == 2 );
-    REQUIRE( foo(2) == 4 );
-    REQUIRE( foo(3) == 6 );
+TEST_CASE("foo will times by 2", "[foo]") {
+    REQUIRE(foo(1) == 2);
+    REQUIRE(foo(2) == 4);
+    REQUIRE(foo(3) == 6);
 }
